b03d9dca31ad0295ef72a2bdc7725e159d3f1fbf
WouterJac <wouter.jacobin@telenet.be>
2024-04-26 01:04:46 +0200

    play_card toegevoegd

--- game_logic.py
+++ game_logic.py
@@ -35,11 +35,10 @@ class GameLogic:
             self.player_hands[player].append(self.deck.pop())
 
     def deal_card(self):
-        self.player_hands[current_player].append(self.deck.pop())
+        self.player_hands[self.current_player].append(self.deck.pop())
 
     def advance_turn(self):
-        global current_player
-        current_player = current_player + 1 % self.num_players
+        self.current_player = self.current_player + 1 % self.num_players
 
     def print_hands(self):
         print("All player cards:")
@@ -47,10 +46,10 @@ class GameLogic:
             self.print_hand(player)
 
     def print_hand(self, player):
-        print("Player ", player, " hand: ")
+        print("\nPlayer ", player, " hand: ")
         hand = ""
         for card in range(len(self.player_hands[player])):
-            hand = hand + str(card+1) + ") " + self.player_hands[player][card]
+            hand = hand + str(card+1) + ") " + self.player_hands[player][card]+" "
         print(hand)
 
     def game_is_over(self):
@@ -61,5 +60,22 @@ class GameLogic:
 
     def play_turn(self):
         self.deal_card()
-        self.print_hand(current_player)
+        self.print_hand(self.current_player)
+        chosen_card = 0
+        card_chosen = False
+        while not card_chosen:
+            try:
+                chosen_card = int(input('Welke kaart wil je spelen? (1/2): '))
+                if chosen_card < 1 or chosen_card > 2:
+                    card_chosen = False
+                    print("Please choose a valid option.\n")
+                else:
+                    card_chosen = True
+            except ValueError:
+                print("Please choose a valid option.\n")
+        self.play_card(int(chosen_card-1))
         self.advance_turn()
+
+    def play_card(self, card):
+        chosen_card_str = str(self.player_hands[self.current_player][card])
+        print(chosen_card_str)
--- main.py
+++ main.py
@@ -7,10 +7,9 @@ games = 1
 # for game in range(games):
 game_logic = game_logic.GameLogic()
 game_logic.print_hands()
-'''
+
 game_over = False
-while not game_over:
-    game_logic.play_turn()
-    game_over = game_logic.game_is_over()
+game_logic.play_turn()
+game_over = game_logic.game_is_over()
 game_logic.end_game()
-'''
+
