f01c07dc34e2ae723f28c57ad9c622bc6d0bda8b
WouterJac <wouter.jacobin@telenet.be>
2024-04-26 13:57:13 +0200

    priest & baron

--- game_logic.py
+++ game_logic.py
@@ -7,6 +7,7 @@ class GameLogic:
         self.players_left = self.num_players
         self.player_hands = []
         self.player_lost = set()
+        self.player_kamermeisje = set()
         self.current_player = 1
         self.keuzes = {
             1: "Wachter",
@@ -18,31 +19,14 @@ class GameLogic:
             7: "Gravin",
             8: "Prinses",
         }
-        self.deck = [
-            "Wachter",
-            "Wachter",
-            "Wachter",
-            "Wachter",
-            "Wachter",
-            "Priester",
-            "Priester",
-            "Baron",
-            "Baron",
-            "Kamermeisje",
-            "Kamermeisje",
-            "Prins",
-            "Prins",
-            "Koning",
-            "Gravin",
-            "Prinses",
-        ]
+        self.deck = [1, 1, 1, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 7, 8]
         random.shuffle(self.deck)
-        for i in range(self.num_players+1):
+        for i in range(self.num_players + 1):
             self.player_hands.append([])
         self.deal_cards()
 
     def deal_cards(self):
-        for player in range(1,self.num_players+1,1):
+        for player in range(1, self.num_players + 1, 1):
             self.player_hands[player].append(self.deck.pop())
 
     def deal_card(self):
@@ -54,13 +38,19 @@ class GameLogic:
     def print_hands(self):
         print("All player cards:")
         for player in range(self.num_players):
-            self.print_hand(player)
+            self.print_hand(player + 1)
 
     def print_hand(self, player):
         print("\nPlayer ", player, " hand: ")
         hand = ""
         for card in range(len(self.player_hands[player])):
-            hand = hand + str(card + 1) + ") " + self.player_hands[player][card] + " "
+            hand = (
+                hand
+                + str(card + 1)
+                + ") "
+                + self.keuzes.get(self.player_hands[player][card])
+                + " "
+            )
         print(hand)
 
     def game_is_over(self):
@@ -77,7 +67,7 @@ class GameLogic:
         print("Player ", player, " has lost and is out of the game.")
 
     def play_turn(self):
-        print("Player ", str(self.current_player), "'s turn.")
+        print("\nPlayer ", str(self.current_player), "'s turn.")
         self.deal_card()
         self.print_hand(self.current_player)
         chosen_card = 0
@@ -96,22 +86,32 @@ class GameLogic:
         self.advance_turn()
 
     def play_card(self, card):
-        chosen_card_str = str(self.player_hands[self.current_player][card])
+        chosen_card = self.player_hands[self.current_player].pop(card)
+        chosen_card_str = self.keuzes.get(chosen_card)
         print(chosen_card_str)
         match chosen_card_str:
             case "Prinses":
                 self.play_princess()
             case "Wachter":
                 self.play_guard()
+            case "Priester":
+                self.play_priest()
 
     def play_princess(self):
         self.player_lose(self.current_player)
+    
+    def player_kamermeisje(self,player):
+        return player in players_kamermeisje
 
-    def play_guard(self):
+    def choose_target_player(self):
         print("Choose a player to target:")
         eligible_players = set()
         for player in range(self.num_players):
-            if not self.player_has_lost(player+1) and player+1 != self.current_player:
+            if (
+                not self.player_has_lost(player + 1)
+                and player + 1 != self.current_player
+                and not self.player_kamermeisje(player+1)
+            ):
                 eligible_players.add(player + 1)
         print(eligible_players)
 
@@ -128,7 +128,16 @@ class GameLogic:
             except ValueError:
                 print("Please choose a valid option.")
                 print(eligible_players)
+        return chosen_player
+    
+    def get_player_card(self, player):
+        return self.player_hands[player][0]
+    
+    def get_current_player_other_card(self):
+        return self.player_hands[self.current_player][0]
 
+    def play_guard(self):
+        target = self.choose_target_player()
         guessed_a_card = False
         while not guessed_a_card:
             try:
@@ -142,5 +151,19 @@ class GameLogic:
                     guessed_a_card = True
             except ValueError:
                 print("Please choose a valid option.\n")
-        if self.player_hands[chosen_player][0] == self.keuzes.get(card_guess):
-            self.player_lose(chosen_player)
+        if self.player_hands[target][0] == self.keuzes.get(card_guess):
+            self.player_lose(target)
+
+    def play_priest(self):
+        target = self.choose_target_player()
+        print("This is the hand of player ", target, ": ")
+        self.print_hand(target)
+    
+    def play_baron(self):
+        target = self.choose_target_player()
+        target_card = self.get_player_card(target)
+        current_player_card = self.get_current_player_other_card()
+        if target_card > current_player_card:
+            self.player_lose(self.current_player)
+        elif target_card < current_player_card:
+            self.player_lose(target)
